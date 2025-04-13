74b5d8aabc2357cd4cedf8fad39d9972058a650e
zavdav <157126752+zavdav@users.noreply.github.com>
2025-04-13 21:06:39 +0200

    Add messages.properties and message utilities

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "me.zavdav.zcore"
-version = "0.2.0"
+version = "0.2.1"
 
 repositories {
     mavenCentral()
---
+++ src/main/kotlin/me/zavdav/zcore/util/Messages.kt
@@ -0,0 +1,25 @@
+package me.zavdav.zcore.util
+
+import java.util.ResourceBundle
+
+private val bundle = ResourceBundle.getBundle("messages")
+
+internal fun getMessage(key: String): String = bundle.getString(key)
+
+internal fun String.colored() = replace("&([0-9a-f])".toRegex(), "§$1")
+
+internal fun tl(key: String, vararg args: Any): String {
+    var message = getMessage(key).colored()
+    for (i in args.indices) {
+        message = message.replace("{$i}", args[i].toString())
+    }
+    return message
+}
+
+internal fun fmt(message: String, vararg pairs: Pair<String, Any>): String {
+    var message = message.colored()
+    for (pair in pairs) {
+        message = message.replace("{${pair.first.uppercase()}}", pair.second.toString())
+    }
+    return message
+}
